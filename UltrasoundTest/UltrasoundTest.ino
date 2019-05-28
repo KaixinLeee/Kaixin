6b18b4feaa79efc9c8e0a7d677b561b17589a2ac
KaixinLeee <50830761+KaixinLeee@users.noreply.github.com>
2019-05-28 22:12:26 +0100

    arduino update

--- UltrasoundTest/UltrasoundTest.ino
+++ UltrasoundTest/UltrasoundTest.ino
@@ -1,6 +1,15 @@
 #include "Ultrasonic.h"
 
 Ultrasonic ultrasonic(7);
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 250 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
 void setup()
 {
 	Serial.begin(115200);
@@ -8,6 +17,8 @@ void setup()
 void loop()
 {
 	byte RangeInCentimeters = constrain (ultrasonic.MeasureInCentimeters(),10,255); // two measurements should keep an interval
-	Serial.write ( (RangeInCentimeters));//0~255cm
+	
+	  Serial.write(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+//Serial.write ( (RangeInCentimeters));//0~255cm
 	delay(250);
 }
